51b61dc9fa0902cc9922eabe628f72c25a2c8aee
jonatas-lopess <jonatinhalopes.011@gmail.com>
2025-04-16 10:56:46 -0300

    feat: add all models api routes

---
+++ src/app/api/score/[id]/route.ts
@@ -0,0 +1,94 @@
+import Score from "@api/models/score";
+import mongoose from "mongoose";
+import { NextResponse } from "next/server";
+import { RouteParams } from "@api/app/api/types";
+
+export async function GET(request: Request, { params }: RouteParams) {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI as string);
+
+    const { id } = await params;
+    if (mongoose.isValidObjectId(id) === false)
+      return NextResponse.json(
+        { message: "Invalid identifier." },
+        { status: 400 }
+      );
+
+    const document = await Score.findById(id);
+
+    return NextResponse.json(document ?? {}, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "An error occurred while processing your request.", error },
+      { status: 500 }
+    );
+  } finally {
+    await mongoose.disconnect();
+  }
+}
+
+export async function PUT(request: Request, { params }: RouteParams) {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI as string);
+
+    const { id } = await params;
+    if (mongoose.isValidObjectId(id) === false)
+      return NextResponse.json(
+        { message: "Invalid identifier." },
+        { status: 400 }
+      );
+
+    const req = await request.json();
+
+    const document = await Score.findById(id);
+    if (document === null)
+      return NextResponse.json(
+        { message: "Score not found." },
+        { status: 404 }
+      );
+
+    document.set(req);
+    const data = await document.save();
+
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "An error occurred while processing your request.", error },
+      { status: 500 }
+    );
+  } finally {
+    await mongoose.disconnect();
+  }
+}
+
+export async function DELETE(request: Request, { params }: RouteParams) {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI as string);
+
+    const { id } = await params;
+    if (mongoose.isValidObjectId(id) === false)
+      return NextResponse.json(
+        { message: "Invalid identifier." },
+        { status: 400 }
+      );
+
+    const document = await Score.findByIdAndDelete(id);
+    if (document === null)
+      return NextResponse.json(
+        { message: "Score not found." },
+        { status: 404 }
+      );
+
+    return NextResponse.json({}, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: "An error occurred while processing your request.",
+        error: error,
+      },
+      { status: 500 }
+    );
+  } finally {
+    await mongoose.disconnect();
+  }
+}

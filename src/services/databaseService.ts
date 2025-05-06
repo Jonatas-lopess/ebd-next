73a1e6f646f7205aa5c89f003171b73020d49bd0
Jonatas-lopess <jonataslopes.011@gmail.com>
2025-05-06 10:13:08 -0300

    refactor: add DatabaseService GenericModelManager

--- src/services/databaseService.ts
+++ src/services/databaseService.ts
@@ -1,146 +1,105 @@
 import mongoose from "mongoose";
 
 type DatabaseParams = {
-  model: mongoose.Model<any>;
-  id?: any;
-  data?: any;
+  id?: mongoose.Types.ObjectId | string;
+  data?: mongoose.RootFilterQuery<any>;
 };
 
-const findAll = async (model: mongoose.Model<any>) => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
-    mongoose.connection.on("error", (error) => {
-      throw error;
-    });
+interface IDatabaseService {
+  readonly model: mongoose.Model<any>;
+  create: (params: DatabaseParams) => Promise<any>;
+  read: (params: DatabaseParams) => Promise<any>;
+  update: (params: DatabaseParams) => Promise<any>;
+  delete: (params: DatabaseParams) => Promise<any>;
+}
 
-    const data = await model.find();
+export default class GenericModelManager implements IDatabaseService {
+  readonly model: mongoose.Model<any>;
 
-    return data;
-  } catch (error) {
-    throw new Error(`Error fetching in model: ${model.modelName}`, {
-      cause: error,
-    });
-  } finally {
-    await mongoose.disconnect();
+  constructor(model: mongoose.Model<any>) {
+    this.model = model;
   }
-};
 
-const findById = async ({ model, id }: DatabaseParams) => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
-    mongoose.connection.on("error", (error) => {
-      throw error;
-    });
+  async create({ data }: DatabaseParams) {
+    try {
+      await mongoose.connect(process.env.MONGODB_URI as string);
+      mongoose.connection.on("error", (error) => {
+        throw error;
+      });
 
-    if (mongoose.isValidObjectId(id) === false)
-      throw new Error("Invalid identifier.");
-
-    const document = await model.findById(id);
-
-    return document;
-  } catch (error) {
-    throw new Error(`Error fetching in model: ${model.modelName}`, {
-      cause: error,
-    });
-  } finally {
-    await mongoose.disconnect();
+      return await this.model.create(data);
+    } catch (error) {
+      throw new Error(`Error creating in model: ${this.model.modelName}`, {
+        cause: error,
+      });
+    } finally {
+      await mongoose.disconnect();
+    }
   }
-};
 
-const findOne = async ({ model, data }: DatabaseParams) => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
-    mongoose.connection.on("error", (error) => {
-      throw error;
-    });
+  async read({ id, data = {} }: DatabaseParams) {
+    try {
+      await mongoose.connect(process.env.MONGODB_URI as string);
+      mongoose.connection.on("error", (error) => {
+        throw error;
+      });
 
-    const document = await model.findOne(data);
+      if (id) return await this.model.findById(id);
 
-    return document;
-  } catch (error) {
-    throw new Error(`Error fetching in model: ${model.modelName}`, {
-      cause: error,
-    });
-  } finally {
-    await mongoose.disconnect();
+      return await this.model.find(data);
+    } catch (error) {
+      throw new Error(`Error fetching in model: ${this.model.modelName}`, {
+        cause: error,
+      });
+    } finally {
+      await mongoose.disconnect();
+    }
   }
-};
 
-const create = async ({ model, data }: DatabaseParams) => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
-    mongoose.connection.on("error", (error) => {
-      throw error;
-    });
+  async update({ id, data }: DatabaseParams) {
+    try {
+      await mongoose.connect(process.env.MONGODB_URI as string);
+      mongoose.connection.on("error", (error) => {
+        throw error;
+      });
 
-    const document = await model.create(data);
+      if (id === undefined) throw new Error("Invalid identifier.");
 
-    return document;
-  } catch (error) {
-    throw new Error(`Error creating in model: ${model.modelName}`, {
-      cause: error,
-    });
-  } finally {
-    await mongoose.disconnect();
+      const document = await this.model.findById(id);
+      if (document === null)
+        throw new Error(`${this.model.modelName} not found.`);
+
+      document.set(data);
+      return await document.save();
+    } catch (error) {
+      throw new Error(`Error updating in model: ${this.model.modelName}`, {
+        cause: error,
+      });
+    } finally {
+      await mongoose.disconnect();
+    }
   }
-};
 
-const update = async ({ model, id, data }: DatabaseParams) => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
-    mongoose.connection.on("error", (error) => {
-      throw error;
-    });
+  async delete({ id }: DatabaseParams) {
+    try {
+      await mongoose.connect(process.env.MONGODB_URI as string);
+      mongoose.connection.on("error", (error) => {
+        throw error;
+      });
 
-    if (mongoose.isValidObjectId(id) === false)
-      throw new Error("Invalid identifier.");
+      if (id === undefined) throw new Error("Invalid identifier.");
 
-    const document = await model.findById(id);
-    if (document === null) throw new Error(`${model.modelName} not found.`);
+      const document = await this.model.findByIdAndDelete(id);
+      if (document === null)
+        throw new Error(`${this.model.modelName} not found.`);
 
-    document.set(data);
-    const updatedDocument = await document.save();
-
-    return updatedDocument;
-  } catch (error) {
-    throw new Error(`Error updating in model: ${model.modelName}`, {
-      cause: error,
-    });
-  } finally {
-    await mongoose.disconnect();
+      return document;
+    } catch (error) {
+      throw new Error(`Error deleting in model: ${this.model.modelName}`, {
+        cause: error,
+      });
+    } finally {
+      await mongoose.disconnect();
+    }
   }
-};
-
-const remove = async ({ model, id }: DatabaseParams) => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI as string);
-    mongoose.connection.on("error", (error) => {
-      throw error;
-    });
-
-    if (mongoose.isValidObjectId(id) === false)
-      throw new Error("Invalid identifier.");
-
-    const document = await model.findByIdAndDelete(id);
-    if (document === null) throw new Error(`${model.modelName} not found.`);
-
-    return document;
-  } catch (error) {
-    throw new Error(`Error deleting in model: ${model.modelName}`, {
-      cause: error,
-    });
-  } finally {
-    await mongoose.disconnect();
-  }
-};
-
-const db = {
-  findAll,
-  findById,
-  findOne,
-  create,
-  update,
-  remove,
-};
-
-export default db;
+}

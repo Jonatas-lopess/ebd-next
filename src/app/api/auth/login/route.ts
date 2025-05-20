c4b80529b80aef4a97f818317d8ee931bae4418a
Jonatas-lopess <jonataslopes.011@gmail.com>
2025-05-20 10:47:36 -0300

    feat: add user GET route

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,8 +1,7 @@
 import { NextResponse } from "next/server";
 import { compare } from "bcrypt-ts";
 import { SignJWT } from "jose";
-import { HydratedDocument } from "mongoose";
-import User, { IUser } from "@api/models/User";
+import User from "@api/models/User";
 
 export async function POST(req: Request) {
   try {
---
+++ src/app/api/user/route.ts
@@ -0,0 +1,32 @@
+import User from "@api/models/User";
+import { NextResponse } from "next/server";
+
+export async function GET(req: Request) {
+  try {
+    const userId = req.headers.get("userId");
+    const db = new User();
+
+    if (!userId) {
+      return NextResponse.json(
+        { message: "Error retrieving userId." },
+        { status: 400 }
+      );
+    }
+
+    const user = await db.read({ id: userId });
+
+    if (!user) {
+      return NextResponse.json({ message: "User not found." }, { status: 404 });
+    }
+
+    return NextResponse.json(
+      { message: "User data found successfully.", user },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "An error occurred while processing your request.", error },
+      { status: 500 }
+    );
+  }
+}
